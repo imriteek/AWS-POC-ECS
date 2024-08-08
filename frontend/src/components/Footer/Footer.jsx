8b9b4f190aea2ceb51924fbfe8caba4b557c389d
Riteek Sharma <riteeksharma97@gmail.com>
2024-08-08 14:36:47 +0530

    Fronted 5173 updated

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -27,15 +27,15 @@ const Footer = () => {
             <div className="footer-content-right">
                 <h2>GET IN TOUCH</h2>
                 <ul>
-                    <li>+94 765489545</li>
-                    <li>dulanjalisenarathna93@gmail.com</li>
+                    <li>+91 9929709273</li>
+                    <li>Riteeksharma97@gmail.com</li>
                 </ul>
             </div>
            
         </div>
         <hr />
         <p className="footer-copyright">
-            Copyright 2024 &copy; Dulanjali - All Right Reserved.
+            Copyright 2024 &copy; Riteek Sharma - All Right Reserved.
         </p>
     </div>
   )
