1a3e697a91d7422670da48a0c601b0a0eae8e786
Riteek Sharma <riteeksharma97@gmail.com>
2024-08-09 16:52:38 +0530

    9th aug

--- before_install.sh
+++ before_install.sh
@@ -2,5 +2,5 @@
 
 # Pull the Docker image from ECR
 echo "Pulling Docker image..."
-docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+docker pull 905418426404.dkr.ecr.ap-south-1.amazonaws.com/imriteek:backend
 
