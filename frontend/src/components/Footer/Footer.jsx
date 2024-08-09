29008557587425231d87a0e5f6031a5a6188b759
Riteek Sharma <riteeksharma97@gmail.com>
2024-08-09 13:36:31 +0530

    9th aug

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
                 <h2>GET IN TOUCH</h2>
                 <ul>
                     <li>+91 9929709273</li>
-                    <li>prateekgoyal97@gmail.com</li>
+                    <li>Riteeksharma97@gmail.com</li>
                 </ul>
             </div>
            
