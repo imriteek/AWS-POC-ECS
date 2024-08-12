13ae697eb2f9a85920d2f751174d334af39d9a63
Riteek Sharma <riteeksharma97@gmail.com>
2024-08-12 18:12:44 +0530

    12th aug

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 905418426404.dkr.ecr.ap-south-1.amazonaws.com
 docker-compose pull
 docker-compose down
 docker-compose up -d
