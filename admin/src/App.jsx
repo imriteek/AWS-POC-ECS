71c33fce56416625c409724f4b3129dce19c700d
Riteek Sharma <riteeksharma97@gmail.com>
2024-08-23 11:50:48 +0530

    23rd aug

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://AWS-POC-ALB-1797031006.ap-south-1.elb.amazonaws.com";
+  const url = "http://aws-poc-ecs-alb-1533352154.ap-south-1.elb.amazonaws.com";
 
   return (
     <div>
