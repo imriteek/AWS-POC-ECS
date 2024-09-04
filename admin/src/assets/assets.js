6bbce8b96a47cd408074afe6c3626063ce1119e6
Riteek Sharma <riteeksharma97@gmail.com>
2024-09-04 13:10:39 +0530

    4th sept

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = "http://aws-poc-ecs-alb-1795605982.ap-south-1.elb.amazonaws.com";
+export const url = "https://aws-poc-ecs-alb-1795605982.ap-south-1.elb.amazonaws.com";
